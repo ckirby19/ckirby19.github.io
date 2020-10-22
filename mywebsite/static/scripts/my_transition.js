c164950aa9eea5da624e96e376aae5a345490c3f
ckirby19 <conorkirby1@gmail.com>
2020-10-23 00:15:35 +0100

    swup changes so page reloads at top

--- mywebsite/static/scripts/my_transition.js
+++ mywebsite/static/scripts/my_transition.js
@@ -1,4 +1,31 @@
-const swup = new Swup();
+const options = {
+    animationSelector: '[class*="transition-fade"]',
+    animateHistoryBrowsing: true,
+    animateScroll: false,
+    plugins: [
+
+        new SwupScrollPlugin({
+            animateScroll: false
+        })
+    ]
+};
+
+
+let scrollValues = {};
+
+const swup = new Swup(options);
+
+
+swup.on('clickLink', () => {
+    scrollValues[window.location.href] = window.scrollY;
+});
+
+swup.on('popState', () => {
+    setTimeout(function() {
+        window.scrollTo(0, scrollValues[window.location.href]);
+    }, 100);
+});
+
 
 function init() {
 
@@ -103,7 +130,3 @@ function init() {
 init();
 document.addEventListener('swup:contentReplaced', init);
 
-// Go to top of page on load
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
