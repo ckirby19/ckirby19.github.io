b369be2b96ff6702ddcb15def5a575db785b8909
ckirby19 <conorkirby1@gmail.com>
2020-11-24 20:09:12 +0000

    changes

---
+++ mywebsite/static/scripts/for_mobile.js
@@ -0,0 +1,10 @@
+//For mobile
+function showLinks() {
+    var x = document.getElementById("invisible");
+    if (x.style.display == "flex") {
+        x.style.display = "none"
+    }
+    else {
+        x.style.display = "flex"
+    }
+}
--- mywebsite/static/scripts/my_transition.js
+++ mywebsite/static/scripts/my_transition.js
@@ -124,6 +124,7 @@ function init() {
         $(this).attr('href', href.replace('badmail.', ''));
     });
 
+    
       
 }
 
