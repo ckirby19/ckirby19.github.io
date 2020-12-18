87e542ef89061e028b98064d373c2c529620c295
ckirby19 <conorkirby1@gmail.com>
2020-12-18 23:33:25 +0000

    Debug Off

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from mywebsite import app #Will import from init file of file
 
 if __name__ == "__main__":
     # create_tables() #create if it does not already exist
-    app.run(debug=True)
+    app.run(debug=False)
